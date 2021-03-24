57eaa5b17e20576096a77fb7d514ecfa07929dfa
Ryder <ryderblue2@gmail.com>
2021-03-24 15:29:03 -0700

    Finished MVP

--- index.js
+++ index.js
@@ -98,10 +98,16 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.favoriteToy = favoriteToy;
+  Person.call(this, name, age);
   }
  
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function (){
+    return `Playing with ${this.favoriteToy}`;
+  }
   
   /* 
     TASK 4
